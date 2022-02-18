57eb93cb468122ceafcef80957f8313687ae5e3e
cmizell186 <chrislmiz311@gmail.com>
2022-02-18 11:37:15 -0800

    finished js & html

---
+++ main.js
@@ -0,0 +1,20 @@
+// get all elements from index.html to make decisions with
+let addToDoButton = document.getElementById('addToList');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputText');
+
+// test to see if working correctly
+console.log(addToDoButton, toDoContainer, inputField);
+
+addToDoButton.addEventListener('click', function(){
+    let paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = 'line-through';
+    })
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
+})
